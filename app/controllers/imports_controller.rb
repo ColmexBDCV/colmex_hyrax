ee259067e7466fcd19a5ed2c3de197dce5515ef1
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2018-05-08 13:28:21 -0500

    2222222222222222222

---
+++ app/controllers/imports_controller.rb
@@ -0,0 +1,12 @@
+class ImportsController < ApplicationController
+ 
+
+
+  # GET /imports
+  # GET /imports.json
+  def index
+    # @imports = @imports.order(created_at: :desc).page(params[:page])
+  end
+
+  
+end
