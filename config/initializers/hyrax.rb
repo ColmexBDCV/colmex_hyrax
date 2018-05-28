dd944e18bbab17150a7379bc705760112f361252
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2018-05-28 16:41:54 -0500

    'Analytics Update'

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -40,15 +40,15 @@ Hyrax.config do |config|
   # Enable displaying usage statistics in the UI
   # Defaults to false
   # Requires a Google Analytics id and OAuth2 keyfile.  See README for more info
-  # config.analytics = false
+  config.analytics = true
 
   # Google Analytics tracking ID to gather usage statistics
-  # config.google_analytics_id = 'UA-99999999-1'
+  config.google_analytics_id = 'UA-13170057-8'
 
   # Date you wish to start collecting Google Analytic statistics for
   # Leaving it blank will set the start date to when ever the file was uploaded by
   # NOTE: if you have always sent analytics to GA for downloads and page views leave this commented out
-  # config.analytic_start_date = DateTime.new(2014, 9, 10)
+  config.analytic_start_date = DateTime.new(2014, 9, 10)
 
   # Enables a link to the citations page for a work
   # Default is false
@@ -80,7 +80,7 @@ Hyrax.config do |config|
   # config.redis_namespace = "hyrax"
 
   # Path to the file characterization tool
-  config.fits_path = ENV['FITS_PATH']
+  config.fits_path = ENV['FITS_PATH'] || "/home/rod/fits-1.2.0/fits.sh"
 
   # Path to the file derivatives creation tool
   config.libreoffice_path = "soffice"
