fcf66ef46ff882a829ead4937e39865e2738821e
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2019-05-08 12:31:17 -0500

    citation way feature

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -2,4 +2,25 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+  
+  def build_citation(presenter)
+    "#{build_authors(presenter)} (#{presenter.date_created.first}), #{presenter} (#{presenter.resource_type.first}), El Colegio de México, Ciudad de México, México"
+  end
+
+  def build_authors(presenter)
+    s = ""
+    presenter.creator.each do |a|
+      n = a.split(', ')
+      s = s + [ n[1], n[0] ].join(" ")+", "
+    end
+
+    presenter.contributor.each do |a|
+      n = a.split(', ')
+      s = s + [ n[1], n[0] ].join(" ")+", "
+    end
+    
+    return s.chomp(", ")
+  end
+  
+
 end
