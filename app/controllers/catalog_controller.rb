6071e6309617653d8d10d4bc8de7812cc1deb8b7
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2019-06-17 16:05:50 -0500

    reorder facets

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -68,12 +68,14 @@ class CatalogController < ApplicationController
     config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
     config.add_facet_field solr_name("creator", :facetable), limit: 5
     config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
-    config.add_facet_field solr_name("director", :facetable), limit: 5
+    config.add_facet_field solr_name("center", :facetable), limit: 5
+    config.add_facet_field solr_name("date_created", :facetable), limit: 5
+    
     # config.add_facet_field solr_name("keyword", :facetable), limit: 5
-    config.add_facet_field solr_name("subject", :facetable), limit: 5
-    config.add_facet_field solr_name("subject_person", :facetable), limit: 5
     config.add_facet_field solr_name("subject_work", :facetable), limit: 5
+    config.add_facet_field solr_name("subject_person", :facetable), limit: 5
     config.add_facet_field solr_name("subject_corporate", :facetable), limit: 5
+    config.add_facet_field solr_name("subject", :facetable), limit: 5
     # config.add_facet_field solr_name("subject_family", :facetable), limit: 5
     # config.add_facet_field solr_name("language", :facetable), limit: 5
     config.add_facet_field solr_name("based_near_label", :facetable), limit: 5
@@ -82,9 +84,8 @@ class CatalogController < ApplicationController
     # config.add_facet_field solr_name("publisher", :facetable), limit: 5
     config.add_facet_field solr_name("file_format", :facetable), limit: 5
     #config.add_facet_field solr_name('member_of_collections', :symbol), limit: 5, label: 'Collections'
-    config.add_facet_field solr_name("center", :facetable), limit: 5
-    config.add_facet_field solr_name("date_created", :facetable), limit: 5
-
+   
+    config.add_facet_field solr_name("director", :facetable), limit: 5
 
     # The generic_type isn't displayed on the facet list
     # It's used to give a label to the filter that comes from the user profile
