ec17d62dacdf3bc6731c3a4444b220f6bc378cc3
rodyoukai <rod.youkai@gmail.com>
2020-04-14 12:20:36 -0500

    disable mode_of_publication in thesis

--- app/forms/hyrax/thesis_form.rb
+++ app/forms/hyrax/thesis_form.rb
@@ -10,7 +10,7 @@ module Hyrax
       :director, :awards, 
       :item_access_restrictions, :item_use_restrictions, :edition, :encoding_format_details, 
       :file_type_details, :digital_resource_generation_information, :file_details, :dimensions, 
-      :system_requirements, :exemplar_of_manifestation, :extension, :type_of_file, :mode_of_publication, 
+      :system_requirements, :exemplar_of_manifestation, :extension, :type_of_file, #:mode_of_publication, 
       :other_formats, :access_restrictions, :use_restrictions, 
       :academic_degree, :type_of_thesis, :degree_program, :institution,  
       :date_of_presentation_of_the_thesis]
--- app/presenters/hyrax/thesis_presenter.rb
+++ app/presenters/hyrax/thesis_presenter.rb
@@ -6,7 +6,8 @@ module Hyrax
     :item_access_restrictions, :item_use_restrictions, 
     :edition, :encoding_format_details, :file_type_details, :digital_resource_generation_information, 
     :file_details, :dimensions, :system_requirements, :exemplar_of_manifestation, :extension, :type_of_file, 
-    :mode_of_publication, :other_formats, :access_restrictions, :use_restrictions, 
+    #:mode_of_publication, 
+    :other_formats, :access_restrictions, :use_restrictions, 
     :academic_degree, :type_of_thesis, :degree_program, :institution,
     #:date_of_presentation_of_the_thesis, 
     to: :solr_document
