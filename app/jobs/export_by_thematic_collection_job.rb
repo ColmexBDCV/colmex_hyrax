4155dbca73c719b87e3e7340401e3a44c23264dd
rodyoukai <rod.youkai@gmail.com>
2021-12-08 10:54:48 -0600

    ExportByThematicCollectionJob fix 2

--- app/jobs/export_by_thematic_collection_job.rb
+++ app/jobs/export_by_thematic_collection_job.rb
@@ -1,7 +1,7 @@
 class ExportByThematicCollectionJob < ApplicationJob
     queue_as :default
     
-    def perform(collkeys)
+    def perform(coll,keys)
       ExporterService.by_thematic_collection(coll,keys)
     end
   end
