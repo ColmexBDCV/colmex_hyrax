3bcd8effb54b170367291c5f3e5744bce058da8a
rodyoukai <rod.youkai@gmail.com>
2022-03-22 11:54:12 -0600

    add relevance asc & desc

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -551,7 +551,8 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     # label is key, solr field is value
-    config.add_sort_field "score desc, #{uploaded_field} desc", label: "relevance"
+    config.add_sort_field "score desc, #{uploaded_field} desc", label: "relevance \u25BC"
+    config.add_sort_field "score desc, #{uploaded_field} asc", label: "relevance \u25B2"
     # config.add_sort_field "#{uploaded_field} desc", label: "date uploaded \u25BC"
     # config.add_sort_field "#{uploaded_field} asc", label: "date uploaded \u25B2"
     config.add_sort_field "#{year_field} desc", label: "date created \u25BC"
