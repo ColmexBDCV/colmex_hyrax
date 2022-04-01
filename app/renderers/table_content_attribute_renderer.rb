e62147659397a24c5e45445d4e22787a1854602f
rodyoukai <rod.youkai@gmail.com>
2022-04-01 10:10:47 -0600

    fix table content render

--- app/renderers/table_content_attribute_renderer.rb
+++ app/renderers/table_content_attribute_renderer.rb
@@ -4,7 +4,6 @@ class TableContentAttributeRenderer < Hyrax::Renderers::AttributeRenderer
   # Special treatment for license/rights.  A URL from the Hyrax gem's config/hyrax.rb is stored in the descMetadata of the
   # curation_concern.  If that URL is valid in form, then it is used as a link.  If it is not valid, it is used as plain text.
   def attribute_value_to_html(value)
-    byebug
     value.gsub("--", "<br>")
   end
 end
