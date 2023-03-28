b14c71bd298d653513a5be9e0ba36d281311f72b
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2023-03-28 15:40:32 -0600

    Extend external_link functionality

--- app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
@@ -2,12 +2,31 @@ module Hyrax
   module Renderers
     class ExternalLinkAttributeRenderer < AttributeRenderer
       private
-
-        def li_value(value)
-          auto_link(value, :html => { :target => '_blank' }) do |link|
-            "<span class='glyphicon glyphicon-new-window'></span>&nbsp;#{link}"
+      
+        def extract_url(text)
+          url_regex = /(https?:\/\/[^\s]+)/
+          match = url_regex.match(text)
+          if match
+            url = match[1]
+            rest_text = text.gsub(url, '')
+            return [url, rest_text.strip]
+          else
+            return nil
           end
         end
+
+        def li_value(value)
+          result = extract_url(value)
+
+          if result
+            "<span class='glyphicon glyphicon-new-window'></span>&nbsp;#{link_to(result[1], result[0], :target => '_blank' ) }"
+          else
+            auto_link(value, :html => { :target => '_blank' }) do |link|
+              "<span class='glyphicon glyphicon-new-window'></span>&nbsp;#{link}"
+            end
+          end
+        end
+    
     end
   end
 end
