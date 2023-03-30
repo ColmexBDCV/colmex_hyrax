c1b18b5660c82e688cd72b80794b8f600bf9e9b8
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2023-03-30 13:25:27 -0600

    add navitagion to table_content in pdf

--- app/renderers/table_content_attribute_renderer.rb
+++ app/renderers/table_content_attribute_renderer.rb
@@ -4,6 +4,14 @@ class TableContentAttributeRenderer < Hyrax::Renderers::AttributeRenderer
   # Special treatment for license/rights.  A URL from the Hyrax gem's config/hyrax.rb is stored in the descMetadata of the
   # curation_concern.  If that URL is valid in form, then it is used as a link.  If it is not valid, it is used as plain text.
   def attribute_value_to_html(value)
-    value.gsub("--", "<br>")
+    match = /\[(\d+)\]/.match(value)
+    if match
+      number = match[1].to_i
+      %(<a onclick="go_to_page(#{match[1]})">#{value.gsub(match[0], "")}</a>)
+    else
+      value.gsub("--", "<br>")
+    end
+    
   end
+  
 end
