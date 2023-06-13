7e1d68ef54a9440df497ed64fa48be9059c88c93
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2023-06-13 14:37:59 -0600

    cut uris external_link_helper

--- app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
@@ -1,8 +1,18 @@
 module Hyrax
   module Renderers
     class ExternalLinkAttributeRenderer < AttributeRenderer
+      require 'uri'
+
       private
-      
+
+        def remove_uri_segments(url)
+          uri = URI.parse(url)
+          uri.path = '/'
+          uri.query = nil
+          uri.fragment = nil
+          uri.to_s
+        end
+
         def extract_url(text)
           url_regex = /(https?:\/\/[^\s]+)|(www\.[^\s]+)/
           match = url_regex.match(text)
@@ -24,8 +34,8 @@ module Hyrax
           if result && result[1] != ""
             "<span class='glyphicon glyphicon-new-window'></span>&nbsp;#{link_to(result[1], result[0], :target => '_blank' ) }"
           else
-            auto_link(value, :html => { :target => '_blank' }) do |link|
-              "<span class='glyphicon glyphicon-new-window'></span>&nbsp;#{link}"
+            auto_link(result[0], :html => { :target => '_blank' }) do |link|
+              "<span class='glyphicon glyphicon-new-window'></span>&nbsp;#{remove_uri_segments(link)}"
             end
           end
         end
