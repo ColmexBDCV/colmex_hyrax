34bb6d6403c4b6759199df3d7d227b49430ac2d3
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2023-06-13 14:16:27 -0600

    Fix external_link_helper

--- app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
@@ -4,11 +4,14 @@ module Hyrax
       private
       
         def extract_url(text)
-          url_regex = /(https?:\/\/[^\s]+)/
+          url_regex = /(https?:\/\/[^\s]+)|(www\.[^\s]+)/
           match = url_regex.match(text)
           if match
-            url = match[1]
+            url = match[0]
             rest_text = text.gsub(url, '')
+            unless url.include? "https://"
+              url = "https://"+url              
+            end
             return [url, rest_text.strip]
           else
             return nil
@@ -18,7 +21,7 @@ module Hyrax
         def li_value(value)
           result = extract_url(value)
 
-          if result
+          if result && result[1] != ""
             "<span class='glyphicon glyphicon-new-window'></span>&nbsp;#{link_to(result[1], result[0], :target => '_blank' ) }"
           else
             auto_link(value, :html => { :target => '_blank' }) do |link|
