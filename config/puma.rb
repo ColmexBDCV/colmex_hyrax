6adbbfb4382733d3a5f208b008968b8073916f82
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2023-07-04 11:30:21 -0600

    add dotenv to puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+require 'dotenv/load'
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -7,7 +8,7 @@
 threads_max = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads_min = ENV.fetch("RAILS_MIN_THREADS") { 1 }
 threads threads_min, threads_max
-
+puts ENV.fetch("URL")
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
