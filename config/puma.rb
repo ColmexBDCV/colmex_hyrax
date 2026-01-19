0632cf7f35b206df68ba7e9eea33c70b9bc56a85
RodYoukai <rod.youkai@gmail.com>
2026-01-19 13:29:06 -0600

    add bind socket

--- config/puma.rb
+++ config/puma.rb
@@ -16,13 +16,15 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+bind "unix:///home/rcuellar/hyrax/shared/sockets/puma.sock"
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 0 }
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -55,3 +57,5 @@ end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+umask 0007
